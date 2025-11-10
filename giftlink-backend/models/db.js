23db39a81a16c96ad89a3e4e3e3fb7d129bee2d7
Ayabonga <mtsoloayabonga@gmail.com>
2025-11-10 17:13:41 -0500

    create register and login pages

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -13,16 +13,11 @@ async function connectToDatabase() {
         return dbInstance
     };
 
-    const client = new MongoClient(url);      
+    const client = new MongoClient(url);
 
-    // Task 1: Connect to MongoDB
     await client.connect();
-
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    const dbInstance = client.db(dbName);
-
-    // Task 3: Return database instance
-    return dbInstance;db
+    dbInstance = client.db(dbName);
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
